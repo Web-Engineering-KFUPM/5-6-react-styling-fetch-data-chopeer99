2180160b5047d5a257af6ae209ff8b655b50d908
chopeer99 <chopeer9999@email.com>
2025-10-15 21:11:20 +0300

    search user + userlist

--- 5-6-react-styling-fetch-data/src/components/SearchBar.jsx
+++ 5-6-react-styling-fetch-data/src/components/SearchBar.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { Form, InputGroup } from 'react-bootstrap'
 
 function SearchBar({ searchTerm, onSearchChange }) {
+  // TODO 1.3: Add bootstrap in Search Bar Component (div tag)
   return (
-    <div className="mb-4">
+    <div className="mb-4"> 
       <InputGroup>
-        <InputGroup.Text></InputGroup.Text>
+        <InputGroup.Text>
+            <i className="bi bi-search"></i> {/* Optional: Add a search icon if you have bootstrap icons */}
+        </InputGroup.Text>
         <Form.Control
           type="text"
+          placeholder="Search users by name..."
           value={searchTerm}
           onChange={(e) => onSearchChange(e.target.value)}
         />
--- 5-6-react-styling-fetch-data/src/components/UserList.jsx
+++ 5-6-react-styling-fetch-data/src/components/UserList.jsx
@@ -3,17 +3,20 @@ import { Row, Col, Alert } from 'react-bootstrap'
 import UserCard from './UserCard'
 
 function UserList({ users, onUserClick }) {
+  // TODO 1.4: Use <Alert variant="info"> when no users are found.
   if (users.length === 0) {
     return (
-      <Alert variant="info">
+      <Alert variant="info" className="mt-3">
         No users found matching your search criteria.
       </Alert>
     );
   }
 
+  // TODO 1.4: Display User Cards in Grid
   return (
     <Row>
       {users.map(user => (
+        // TODO 1.4: Add properties to Col tag: md={6} lg={4} mb-4
         <Col key={user.id} md={6} lg={4} className="mb-4">
           <UserCard user={user} onUserClick={onUserClick} />
         </Col>
